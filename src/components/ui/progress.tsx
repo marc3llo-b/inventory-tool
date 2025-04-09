ca8e92515530cdf38ded8028aef074355a0bef68
marc3llo-b <maesi_bachme@hotmail.com>
2025-04-09 23:38:04 +0200

    Update progress.tsx

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,1 +1,9 @@
+export const Progress = ({ value }: { value: number }) => (
+  <div className="w-full bg-gray-200 rounded">
+    <div
+      className="bg-green-500 h-2 rounded"
+      style={{ width: `${value}%` }}
+    ></div>
+  </div>
+);
 
